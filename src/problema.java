79d6e629d5b4d0872cd17db3cd3057b22cf1e987
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-15 09:42:24 -0300

    Leitura da escolaridade

---
+++ src/problema.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problema {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		int nivelEscolaridade;
+		
+		System.out.println("Qual sua escolaridade");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
+		nivelEscolaridade = input.nextInt();
+		
+		
+		input.close();
+
+	}
+
+}
